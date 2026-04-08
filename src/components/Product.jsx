b5e3839efb897949697f934475b8badc070343f7
BrRoman <nicolas.bancillon@gmail.com>
2026-04-08 05:41:02 +0200

    Products: minor corr.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,15 +4,15 @@ function Product(props) {
   const { product } = props;
 
   return (
-    <div className="product">
+    <>
       <div className="image"><img src={"img/" + product.reference + ".png"} alt="Missing file" /></div>
       <div className="description">
         <p>Referencia {product.reference}</p>
         <p>{product.description}</p>
         <p>Dimensiones: {product.talla}</p>
-        <p>{product.price.toString()} €</p>
+        <p>Precio: {product.price.toString()} €</p>
       </div>
-    </div>
+    </>
   )
 }
 
