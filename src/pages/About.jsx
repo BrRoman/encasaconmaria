cbb9bd37ea1309fab428a18966fd023445e31986
BrRoman <nicolas.bancillon@gmail.com>
2026-04-08 05:39:24 +0200

    About: class => className.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ function About() {
       <Menu />
       <div id="about">
         <h2>¿Quienes somos?</h2>
-        <div class="foto_text">
+        <div className="foto_text">
           <img src={nico_lady} alt="Foto" width="300px" />
           <div className="text">Somos una pareja joven, católica, amantes de la naturaleza. Buscamos hacer las cosas con amor y vivir honestamente del trabajo de nuestras manos, multiplicando la creatividad que Dios ha puesto en nosotros.
             Vivimos en Totana (Murcia), España.</div>
