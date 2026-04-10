4f9fc3c32fd92a1804df8072cb4b17ebed9c5394
BrRoman <nicolas.bancillon@gmail.com>
2026-04-10 07:42:39 +0200

    Contact: add spinning cog.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import './Contact.css'
 import Menu from '../components/Menu'
 import constructionImg from '../assets/img/construction.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Contact() {
   return (
@@ -8,7 +9,10 @@ function Contact() {
       <Menu />
       <div id="contact">
         <h1>Contáctanos</h1>
-        <h3>Pagina en construcción…</h3>
+        <div className="d-flex align-items-center bg-light text-primary border rounded my-3 p-3">
+          <FontAwesomeIcon icon="fa-solid fa-cog" spin size="3x" />
+          <h3 className="fw-bold ms-2">Pagina en construcción…</h3>
+        </div>
         <img src={constructionImg} alt="Obra" width="400px" />
       </div>
     </>
