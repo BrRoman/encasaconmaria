da2af21f1bfa18db1402d935ab7c9cd6eb5c52ad
BrRoman <nicolas.bancillon@gmail.com>
2026-09-19 15:14:23 +0200

    About page improved.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,38 +1,38 @@
+import Menu from "../components/Menu";
 import Image from "next/image";
 import imgNicoLady from "../../static/img/nico_lady.png";
 
 export default function About() {
   return (
-    <div id="about">
-      <h1>¿Quiénes somos?</h1>
+    <>
+      <Menu />
 
-      <div className="flex flex-col md:flex-row gap-8 my-8 px-4">
-        <Image
-          src={imgNicoLady}
-          alt="Foto"
-          className="w-full md:w-1/2 md:rounded-md img-fluid"
-        />
+      <div id="about">
+        <h1>¿Quiénes somos?</h1>
 
-        <div className="text">
-          Somos una pareja joven, católica, amantes de la naturaleza. Buscamos
-          hacer las cosas con amor y vivir honestamente del trabajo de nuestras
-          manos, multiplicando la creatividad que Dios ha puesto en nosotros.
-          Vivimos en Totana (Murcia), España.
+        <div className="flex-column gap-8 my-8 md:w-2/3 md:mx-auto md:flex-row md:flex">
+          <Image
+            src={imgNicoLady}
+            alt="Foto"
+            width={450}
+            height={450}
+            className="w-full md:max-w-64 md:rounded-md md:mx-auto"
+          />
+
+          <div className="text">
+            Somos una pareja joven, católica, amantes de la naturaleza. Buscamos
+            hacer las cosas con amor y vivir honestamente del trabajo de
+            nuestras manos, multiplicando la creatividad que Dios ha puesto en
+            nosotros. Vivimos en Totana (Murcia), España.
+          </div>
+
+          <p>
+            Nuestra misión es proporcionar productos que ayuden a nuestros
+            clientes a vivir una vida más saludable y equilibrada, mientras
+            fomentamos valores cristianos y promovemos la conexión con la fe.
+          </p>
         </div>
-
-        <p>
-          En casa con María es una tienda cristiana de productos naturales que
-          busca ofrecer a nuestros clientes artículos de calidad que promuevan
-          el bienestar físico, emocional y espiritual. Nos esforzamos por
-          brindar un servicio excepcional y crear un ambiente acogedor para
-          todos los que nos visitan.
-        </p>
-        <p>
-          Nuestra misión es proporcionar productos que ayuden a nuestros
-          clientes a vivir una vida más saludable y equilibrada, mientras
-          fomentamos valores cristianos y promovemos la conexión con la fe.
-        </p>
       </div>
-    </div>
+    </>
   );
 }
